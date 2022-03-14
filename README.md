6d46ce92e0025ffb7806618691be73a700bd49d1
MrHelmet96 <alejandromatiascasco@gmail.com>
2022-03-14 18:29:44 -0300

    First commit. Added a basic HTML and a PseudoCode of how I think I should solve this project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # R-P-S.GhProject
 This is my first JavaScript Project fot The Odin Project Curriculum. It consist on a Rock, Paper, Scissors machine.
+
+In here I would try to update every relevant step of the process. That way I would be able to keep track of my mistakes and the things I learn in the process.
+AT FIRST THERE WOULD BE NO HTML OR CSS, THE FUNCTIONALITY WILL BE SEEN AT THE CONSOLE.
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// The Rock - Paper - Scissors Machine //
+
+// Pseudo Coding
+
+// The user should input an option (Rock, Paper, Scissors)
+            //* Made a basic select -> option for this *//
+// Set up the options for the computer (Rock, Paper, Scissors)
+// Make the computer select one of this options at random
+// Play the first round
+// Determinate who wins
+// display the message (win / lost)
+// Repeat
