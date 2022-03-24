18c88a0c01a907767d020cc4d6636b68b7662dbe
MrHelmet96 <alejandromatiascasco@gmail.com>
2022-03-24 20:36:15 -0300

    third commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,55 +2,68 @@
 
 // Pseudo Coding
 
-// The user should input an option (Rock, Paper, Scissors)
-const playerSelection = document.getElementById('rock');
-                        // document.getElementById('paper');
-                        // document.getElementById('scissors');
+let playerScore = 0;
+let computerScore = 0;
 
-// Set up the options for the computer (Rock, Paper, Scissors)
-        // const options = ["Rock", "Paper", "Scissors"]
-
-// Make the computer select one of this options at random
-function computerPlay() {
-    const options = ['rock', 'paper', 'scissors'];
-    let getOptions = Math.floor(Math.random() * options.length);
-    
-    return options[getOptions]
-}
-
+const playerSelection = playerPlay();
 const computerSelection = computerPlay();
 
-// Play the first round
-function playRound(playerSelection, computerSelection) {
-   console.log(`you selected ${playerSelection}`);
-   console.log(`Computer selected ${computerSelection}`);
-
-   // Determinate who wins and display the message
-
-    //Tie
-   if (playerSelection === computerSelection) {
-       console.log(`iT'S A TIE! You and the Computer choose the same option.`)
-   }
-   //Rock
-     else if ( playerSelection === 'rock' && computerSelection === 'paper') {
-       console.log(`You lose! Paper beats Rock.`)
-   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-       console.log(`You win! Rock beats Scissors.`)
-   }
-   //Paper
-    else if ( playerSelection === 'paper' && computerSelection === 'rock') {
-    console.log(`You win! Paper beats Rock.`)
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    console.log(`You lose! Scissors beats Paper.`)
-    }
-    //Scissors
-    else if ( playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log(`You win! Rock beats Paper.`)
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log(`You lose! Rock beats Scissors.`)
-    }
-
+// Set up the options for the computer (Rock, Paper, Scissors) then make the computer select one of this options at random
+function computerPlay() {
+    const computerOptions = ['rock', 'paper', 'scissors'];
+    return computerOptions[Math.floor(Math.random() * computerOptions.length)];
 }
 
+// The user should input an option (Rock, Paper, Scissors)
+function playerPlay() {
+    const input = prompt("Choose between: Rock - Paper - Scissors");
+    const playerChoice =  input.toLowerCase();
+    return playerChoice;
+}
 
-// Repeat
+console.log(`you selected ` + playerSelection);
+console.log(`Computer selected ` + computerSelection);
+
+// Play the first round  
+function playRound(playerSelection, computerSelection) {
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        playerScore += 1
+        console.log('You win! ' + playerSelection + ' beats ' + computerSelection)
+        console.log("Player score: " + playerScore);    
+        console.log("Computer score: " + computerScore);    
+
+        if (playerScore == 5) {
+            console.log('You won the game! Reload the page to play again');
+            
+        }
+    }
+    else if (playerSelection == computerSelection) {
+        console.log('It\'s a tie. You both chose ' + playerSelection)
+        console.log("Player score: " + playerScore);    
+        console.log("Computer score: " + computerScore);    
+    }
+    else {
+        computerScore += 1
+        console.log('You lose! ' + computerSelection + ' beats ' + playerSelection)
+        console.log("Player score: " + playerScore);    
+        console.log("Computer score: " + computerScore);
+
+        if (computerScore == 5) {
+            console.log('<br><br>I won the game! Reload the page to play again');
+        }
+    }
+}
+
+ playRound(playerSelection, computerSelection);
+
+ // Repeat 5 times
+
+function Game(playRound) {
+    for (let i = 0; i < 5; i++) {
+
+    }
+}
